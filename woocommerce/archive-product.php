3a45d73d97ae5aa6b8fb7ee5a1d484637826b4f7
josecaos <jazzvoon@gmail.com>
2020-01-25 13:13:10 -0600

    Plantas abajo del calendario

--- woocommerce/archive-product.php
+++ woocommerce/archive-product.php
@@ -64,6 +64,11 @@ get_header('shop');
     </div>
   </div>
 
+  <div class="calendario-plantas col-12">
+    <div class="calendario-plantas-cont-img col-12 ">
+      <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/YAYA_plantas.2.png" alt="YayaBeach Club Mahahual">
+    </div>
+  </div>
 </section>
 
 <section id="mapa_playa" class="ungrid container-fluid">
@@ -80,29 +85,29 @@ get_header('shop');
     'posts_per_page'=> 28,
     'orderby'=> 'default',
     'product_cat'=> 'yaya-spots'
-    );
-      $q = new WP_Query($args);
-      // $q = wc_get_products($args);
-      if ($q->have_posts()):
-        ?>
-        <ul class="products">
-          <?php
+  );
+  $q = new WP_Query($args);
+  // $q = wc_get_products($args);
+  if ($q->have_posts()):
+    ?>
+    <ul class="products">
+      <?php
 
-          while ($q->have_posts()):$q->the_post();
-          // var_dump($post->post_title);
-          // var_dump($post);
-          wc_get_template_part('content','product');
+      while ($q->have_posts()):$q->the_post();
+      // var_dump($post->post_title);
+      // var_dump($post);
+      wc_get_template_part('content','product');
 
-        endwhile;
-        ?>
-      </ul>
+    endwhile;
+    ?>
+  </ul>
 
-      <div class="header-mar col-12 imgLiquid imgLiquidFill">
-        <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/YAYA_mar.chico.png" alt="YayaBeach Club Mahahual">
-      </div>
+  <div class="header-mar col-12 imgLiquid imgLiquidFill">
+    <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/YAYA_mar.chico.png" alt="YayaBeach Club Mahahual">
+  </div>
 
-    </section>
-    <?php
-  endif;
-  //
-  get_footer('shop');
+</section>
+<?php
+endif;
+//
+get_footer('shop');
